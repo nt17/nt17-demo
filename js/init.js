a5755451455b3017fee9f748938206d01914983c
Arnaldo Gomes <agnoldo@gmail.com>
2016-08-09 11:30:48 -0300

    Versao inicial

---
+++ js/init.js
@@ -0,0 +1,35 @@
+/*
+	Synchronous by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+skel.init({
+	prefix: 'css/style',
+	resetCSS: true,
+	boxModel: 'border',
+	grid: {
+		gutters: 50
+	},
+	breakpoints: {
+		'mobile': {
+			range: '-480',
+			lockViewport: true,
+			containers: 'fluid',
+			grid: {
+				collapse: true,
+				gutters: 10
+			}
+		},
+		'desktop': {
+			range: '481-',
+			containers: 1200
+		},
+		'1000px': {
+			range: '481-1200',
+			containers: 960
+		}
+	}
+}, {
+
+});
