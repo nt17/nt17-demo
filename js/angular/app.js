2b207f04e3255139c1c502b730d26655dd71d989
patiaguiaroliveira <patiaguiaroliveira@gmail.com>
2017-02-18 12:50:53 -0200

    Update app.js

--- js/angular/app.js
+++ js/angular/app.js
@@ -293,9 +293,14 @@
         },
 */
         CONFERENCE_PROGRAM: {
-            name: 'Program',
+            name: 'Scientific Program',
             id: "conference_program",
             link: 'conference-program.html'
+        },
+	SOCIAL_PROGRAM: {
+            name: 'Social Program',
+            id: "social_program",
+            link: 'social-program.html'
         },
         COMMITTEES: {
             name: 'Committees',
