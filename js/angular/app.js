834db036766a0b4e36e34fab4817d07a82bb813d
patiaguiaroliveira <patiaguiaroliveira@gmail.com>
2017-03-14 19:45:06 -0300

    Update app.js

--- js/angular/app.js
+++ js/angular/app.js
@@ -295,7 +295,7 @@
             link: 'important-dates.html'
         },
         REGISTRATION: {
-            name: 'Registration/Payment, Invitation Letter and Abstract Submission',
+            name: 'Registration/Payment and Abstract Submission',
             id: "registration",
             link: 'registration.html'
         },
